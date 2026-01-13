cefdb097a9c5b16df8c3fae5ac5d5d98882432f9
Alman D. Gonzalez Pogromsky G <research@alman-os.com>
2026-01-13 06:25:22 -0500

    Initial Commit

---
+++ RaccoonSanitizer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RaccoonSanitizer
+//
+//  Created by Tom Riddle on 1/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RaccoonSanitizer/RaccoonSanitizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaccoonSanitizerApp.swift
+//  RaccoonSanitizer
+//
+//  Created by Tom Riddle on 1/13/26.
+//
+
+import SwiftUI
+
+@main
+struct RaccoonSanitizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
